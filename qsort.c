b0fbd1761c73a21d3909dae9ccfb4f27523743d8
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-22 21:55:50 +0000

    qsort currently broken

--- qsort.c
+++ qsort.c
@@ -8,8 +8,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAXLINES 100
+#define MAXLINES 5
 #define MAXLEN 100
+#define ALLOCSIZE 10000
+
+static char allocbuf[ALLOCSIZE];
+static char *allocp = allocbuf;
 
 char *lineptr[MAXLINES];
 
@@ -17,8 +21,8 @@ int readlines(char *lineptr[], int nlines);
 void writelines(char *lineptr[], int nlines);
 void qsort(char *lineptr[],int left, int right);
 
-int getline(char *, int);
-char * alloc(int);
+int zgetline(char *, int);
+char * zalloc(int);
 
 int main(){
   int nlines;
@@ -40,8 +44,8 @@ int readlines(char *lineptr[], int maxlines){
   char *p, line[MAXLEN];
 
   nlines = 0;
-  while ((len = getline(line, MAXLEN)) > 0){
-    if (nlines == maxlines || (p = alloc(len)) == NULL){
+  while ((len = zgetline(line, MAXLEN)) > 0){
+    if (nlines == maxlines || (p = zalloc(len)) == NULL){
       return -1;
 
     } else {
@@ -59,4 +63,50 @@ void writelines(char *lineptr[], int nlines){
   for (i=0;i<nlines;i++){
     printf("%s\n",lineptr[i]);
   }
+}
+
+void qsort(char *v[], int left, int right){
+  int i, last;
+  void swap(char *v[], int i, int j);
+
+  if (left >= right)
+    return;
+  swap(v, left, (left+right)/2);
+  last = left;
+  for (i = left+1; i <= right; i++){
+    if (strcmp(v[i], v[left]) < 0)
+      swap(v, ++last, i);
+  swap(v, left, last);
+  qsort(v, left, last-1);
+  qsort(v, last+1, right);
+  }
+}
+
+void swap(char *v[], int i, int j){
+  char *temp;
+
+  temp = v[i];
+  v[i] = v[j];
+  v[j] = temp;
+}
+
+int zgetline(char s[], int lim){
+  int c, i;
+  i = 0;
+
+  while (--lim > 0 && (c=getchar()) != EOF && c)
+    s[i++] = c;
+  if (c == '\n')
+    s[i++] = c;
+  s[i] = '\0';
+  return i;
+}
+
+char *zalloc(int n){
+   
+   if (allocbuf + ALLOCSIZE - allocp >= n){
+      allocp += n;
+      return allocp - n;
+   } else
+      return 0;
 }
