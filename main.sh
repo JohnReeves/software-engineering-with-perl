ff40e3ccf91242f8998b081671b227bea1bb3446
John Reeves <johnreeves.mathsandcomputing@gmail.com>
2019-12-23 08:11:48 +0000

    dumpflags

--- main.sh
+++ main.sh
@@ -1,3 +1,8 @@
-echo Hello World
-gcc -o shout shout.c
+./shout dumptree output and 
+./shout how to make your own gcc plugins
+# http://starynkevitch.net/basile/gcc-melt/GCC-MELT-HiPEAC2012.pdf
+# https://gcc.gnu.org/onlinedocs/gccint/Plugins.html
+# https://www.fsl.cs.sunysb.edu/ssw/files/Download/gccplugins.pdf
+gcc -fdump-tree-gimple -o shout shout.c
 gcc -o qsort qsort.c
+ 
